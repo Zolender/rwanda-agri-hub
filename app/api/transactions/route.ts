e11644de34eaf11cadcd04b20b02b3553c5a902c
Zolender <ndeingare@gmail.com>
2026-02-15 14:55:40 +0200

    feat: finish up api's get method logic

--- app/api/transactions/route.ts
+++ app/api/transactions/route.ts
@@ -10,10 +10,41 @@ const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL})
 const adapter = new PrismaPg(pool);
 const prisma = new PrismaClient({adapter})
 
-export async function GET(params:NextRequest) {
+export async function GET(request:NextRequest) {
     try{
-        //it first try fetching with related data
+        //we need now to define the filtering methods and so, upgrading this function
+        const searchParams = request.nextUrl.searchParams;
+        const region = searchParams.get('region')
+        const movementType = searchParams.get('movementType')
+        const productCode = searchParams.get('productCode')
+        const startDate = searchParams.get('startDate')
+        const endDate = searchParams.get('endDate')
+
+        //filter creation
+        const where: any = {};
+
+        if(region){
+            where.region = { name: region}
+        }
+        if(movementType){
+            where.movementType =  movementType
+        }
+        if(productCode){
+            where.product = { productCode: productCode}
+        }
+        if(region){
+            where.region = { name: region}
+        }
+        if(startDate || endDate){
+            where.transactionDate = {}
+            if(startDate) where.transactionDate.gte = new Date(startDate)
+            if(endDate)where.transactionDate.lte = new Date(endDate)
+        }
+
+
+
         const transactions = await prisma.transaction.findMany({
+            where: where,
             take: 20,
             orderBy: { transactionDate: 'desc'},
             include: {
@@ -27,6 +58,7 @@ export async function GET(params:NextRequest) {
         return NextResponse.json({
             success: true,
             count: transactions.length,
+            filters: {region, movementType, productCode, startDate, endDate},
             data: transactions
         })
     } catch(error){
