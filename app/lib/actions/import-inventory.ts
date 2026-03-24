8e30d65fc406a8e9d4fddd31e0556c0ba4597151
Zolender <ndeingare@gmail.com>
2026-03-24 23:07:20 +0200

    feat: enforce quantityOrderedUnits as required in Transaction model

--- app/lib/actions/import-inventory.ts
+++ app/lib/actions/import-inventory.ts
@@ -117,6 +117,7 @@ export async function importInventoryAction(data: any[]){
                     })
                 }
             }
+        
             revalidatePath("/dashboard")
             revalidatePath("/transactions")
             return {
