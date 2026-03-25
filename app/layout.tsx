7ea2cffce26652cb38c50e92ab00355dd8a74a08
Zolender <ndeingare@gmail.com>
2026-03-25 23:25:44 +0200

    refactor: reorganize layout structure and reintroduce middleware for authentication

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,33 +7,34 @@ import {Providers} from "./(app)/components/provider";
 const geistSans = Geist({
     variable: "--font-geist-sans",
     subsets: ["latin"],
-    });
+});
 
-    const geistMono = Geist_Mono({
+const geistMono = Geist_Mono({
     variable: "--font-geist-mono",
     subsets: ["latin"],
-    });
+});
 
-    export const metadata: Metadata = {
+export const metadata: Metadata = {
     title: "Rwanda Agri Hub",
     description: "A app design to help out agri sellers",
-    };
+};
 
-    export default function RootLayout({
+export default function RootLayout({
     children,
-    }: Readonly<{
+}: Readonly<{
     children: React.ReactNode;
-    }>) {
+}>) {
     return (
         <html lang="en">
-        <body
-            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
-            <Providers>
-                {children}    
-            </Providers>
-            <Toaster position="top-center" richColors />
-        </body>
+            <body
+                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+                suppressHydrationWarning 
+            >
+                <Providers>
+                    {children}    
+                </Providers>
+                <Toaster position="top-center" richColors />
+            </body>
         </html>
     );
 }
